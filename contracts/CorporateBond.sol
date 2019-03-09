395a4c4275ebe7b66985cc672435f00ce7f98882
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2019-03-09 23:15:57 +0100

    full contract

--- contracts/CorporateBond.sol
+++ contracts/CorporateBond.sol
@@ -1,40 +1,109 @@
 pragma solidity  >=0.5.0 <0.6.0;
-
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol ";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
 contract CorporateBond is Ownable {
-  struct Bond {}
-  mapping (uint256 => address) private _tokenOwner;
+  using ECDSA for bytes32;
+  
+  struct Bond {
+    address payable owner;
+    uint principal;
+    uint maturity; // block
+    uint rate; // basis points over LIBOR
+    address payable counterparty; 
+    uint lastKnownAmount;
+    uint endClosingTime;
+  }
+  // mapping (address => mapping (address => uint)) outstandingCredit;
 
-  event Issued(address indexed _operator, address indexed _to, uint256 _value, bytes _data);
-  event Redeemed(address indexed _operator, address indexed _from, uint256 _value, bytes _data);
+  Bond[] public bonds;
+  uint nbBonds;
+
+  event Issued( uint _principal,uint _maturity, uint _rate);
+  event Redeemed(uint indice);
 
   constructor() public {
   }
-  // Approximation of the ERC1594 Security Token Standard Implementation
-  // Token Issuance
-  function isIssuable() external view returns (bool){
+
+  function calculateDebt(uint amount,uint rate) public pure returns (uint){
+    //TODO call for libor
+    return amount* (1+rate);
+  }
+
+  function makeMessage(uint ind,uint lastAmount) public pure returns (bytes32){
+    return keccak256(abi.encodePacked(ind,lastAmount));
+  }
+  // Token Issuance - Approximate Compliance to ERC 1594: Core Security Token Standard
+  function isIssuable() external pure returns (bool){
     return true;
   }
-  function issue(address _tokenHolder, uint256 _amount uint256 _maturity, uint256 _rate) external{
-    emit Issued(_tokenHolder,maturity,rate)
+  function issue( uint principal, uint maturity, uint rate) external onlyOwner{
+    bonds.push(Bond(msg.sender,principal,maturity,rate,address(0),0,0));
+    nbBonds +=1;
+    emit Issued(principal,maturity,rate);
   }
-
-
 
   // Token Redemption
-  function redeem(uint256 _value, bytes _data) external{
-
+  function redeem(uint ind) external payable{
+    require(msg.sender == bonds[ind].owner || msg.sender==bonds[ind].counterparty );
+    require(bonds[ind].maturity < now);
+    uint currentDebt = calculateDebt(bonds[ind].principal,bonds[ind].rate);
+    require(currentDebt <= msg.value);
+    address(bonds[ind].owner).transfer(currentDebt);
+    delete bonds[ind];
+    emit Redeemed(ind);
   }
 
-	EOY payment > pull libor, get it to you
-	CurrentMeldnder
-	ongoingDebt
+  function transfer(uint ind, address payable to) public{
+    require(msg.sender== bonds[ind].owner);
+    bonds[ind].owner = to;
+  }
 
-	Collateralize(lender)
-	payback(bondID) + amount (pay + interestrate)
-	Blocklockeds
+  function collateralize(uint ind, address payable counterpart)public {
+    require(msg.sender==bonds[ind].owner);
+    require(bonds[ind].counterparty == address(0));
+    bonds[ind].counterparty = counterpart;
+    // outstandingCredit[msg.sender][counterpart] += bonds[ind].principal;
+  }
 
-	calculate intereste(Rate,initblock, finalblock)
 
+  function closing(uint ind, uint lastAmount) public payable {
+    Bond storage bond = bonds[ind];
+    require(bond.owner == msg.sender);
+    require(msg.value>=lastAmount);
+    bond.lastKnownAmount = lastAmount;
+    bond.endClosingTime = now + 36000;
+  }
+
+
+  // After closing, counterparty must withdraw or chanllenge
+  function challengeClosing(uint ind, uint lastAmount, bytes memory signature)  public {
+    Bond storage bond = bonds[ind];
+    require(bond.counterparty == msg.sender);
+    require(lastAmount>bond.lastKnownAmount,"Claim is Useless");
+    //Verify signature of message
+    bytes32 message = makeMessage(ind,lastAmount);
+    address signer = message.toEthSignedMessageHash().recover(signature);
+    require(signer == msg.sender);
+    // get Money and get bond
+    bond.counterparty.transfer(lastAmount);
+    bond.owner = bond.counterparty;
+    bond.counterparty = address(0);
+  }
+
+  function acceptClosing(uint ind)  public {
+    Bond storage bond = bonds[ind];
+    require(bond.counterparty == msg.sender);
+    require(bond.lastKnownAmount>0,"channel is not closing");
+    //get money and release bond
+    bond.counterparty.transfer(bond.lastKnownAmount);
+    bond.counterparty = address(0);
+  }
+
+  function forceBondRelease(uint ind) public {
+    Bond storage bond = bonds[ind];
+    require(bond.owner == msg.sender);
+    require(now>bond.endClosingTime);
+    bond.counterparty=address(0);
+  }
 }
